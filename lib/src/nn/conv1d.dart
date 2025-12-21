d5ebb084d4d25a1a5a6dd7c252c492bd3d7a440b
Ravi Teja Gudapati <tejainece@gmail.com>
2025-12-21 07:57:31 +0100

    Conv1D

---
+++ lib/src/nn/conv1d.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+
+import 'package:tensor/tensor.dart';
+
+import '../tensor/tensor.dart';
+import '../safetensor/safetensor.dart';
+import 'module.dart';
+
+/// 1D-convolutional layer as defined by Radford et al. for OpenAI GPT (and also used in GPT-2).
+/// Effectively a Linear layer with transposed weights.
+class Conv1D extends Module implements SimpleModule {
+  /// [weight] is of shape (numOutChannels, numInChannels, kernelSize)
+  Tensor weight;
+
+  /// [bias] is of shape (numOutChannels)
+  Tensor? bias;
+
+  Conv1D({required super.name, required this.weight, this.bias});
+
+  @override
+  /// [input] is of shape (batch, seq_length, numInChannels)
+  /// [output] is of shape (batch, seq_length, numOutChannels)
+  Tensor forward(Tensor input, {required Context context}) {
+    context.onloadModule(this);
+
+    Tensor output = input.matmul(weight);
+    if (bias != null) {
+      output = output + bias;
+    }
+    return output;
+  }
+
+  @override
+  void resetParameters({Generator? generator}) {
+    Init.kaimingUniform_(weight, a: sqrt(5), generator: generator);
+    if (bias != null) {
+      final fan = Init.calculateKaimingFan(weight);
+      if (fan.fanIn != 0) {
+        double bound = 1.0 / sqrt(fan.fanIn);
+        bias!.uniform_(from: -bound, to: bound, generator: generator);
+      }
+    }
+  }
+
+  @override
+  late final Iterable<Tensor> parameters = [weight, if (bias != null) bias!];
+
+  int get numInChannels => weight.shape[1];
+  int get numOutChannels => weight.shape[0];
+  int get kernelSize => weight.shape[2];
+
+  @override
+  Map<String, dynamic> get meta => {
+    'numInChannels': numInChannels,
+    'numOutChannels': numOutChannels,
+    'kernelSize': kernelSize,
+  };
+
+  @override
+  late final Iterable<Module> submodules = [];
+
+  static Conv1D make({
+    required String name,
+    required int numInChannels,
+    required int numOutChannels,
+    int kernelSize = 1,
+    /* TODO int stride = 1,
+    int padding = 0,
+    int dilation = 1,
+    int groups = 1,
+    bool hasBias = true,
+    PadMode? padMode,
+    Generator? generator,
+    DataType? dataType,
+    Device? device,*/
+  }) {
+    final weight =
+        Tensor.randn([numOutChannels, numInChannels, kernelSize]) * 0.02;
+    final bias = Tensor.zeros([numOutChannels]);
+
+    return Conv1D(name: name, weight: weight, bias: bias);
+  }
+
+  static Future<Conv1D> loadFromSafeTensor(
+    SafeTensorLoader loader, {
+    required String prefix,
+    required String name,
+  }) async {
+    final weight = await loader.loadByName('${prefix}weight');
+    final bias = await loader.loadByName('${prefix}bias');
+
+    return Conv1D(name: name, weight: weight, bias: bias);
+  }
+}
